3837f8cd9e0f173c14fb8b8d04698237e8045b51
jorazaki2 <78829119+jorazaki2@users.noreply.github.com>
2022-08-17 20:56:39 -0500

    Add files via upload

---
+++ OtroImprimir/src/otroimprimir/OtroImprimir.java
@@ -0,0 +1,14 @@
+package otroimprimir;
+
+public class OtroImprimir {
+
+    public static void main(String[] args) {
+        
+        Imprimir i = new Imprimir();
+        
+        i.imprime("Metodo diferente para imprimir");
+        
+        
+    }
+    
+}
